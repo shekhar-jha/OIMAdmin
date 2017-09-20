be6795882b0a2b54f7c2b32ac2e399a9bc0f42e2
Shekhar Jha <sh3khar.jha@gmail.com>
2017-09-20 13:31:09 -0500

    [REFACTOR] Rewrite Mac OS X code to allow compilation on windows

--- src/main/java/com/jhash/oimadmin/ui/utils/MacOSXHandler.java
+++ src/main/java/com/jhash/oimadmin/ui/utils/MacOSXHandler.java
@@ -16,11 +16,12 @@
 package com.jhash.oimadmin.ui.utils;
 
 import com.jhash.oimadmin.ui.OIMAdmin;
-
-import javax.swing.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MacOSXHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(MacOSXHandler.class);
     public static boolean isMac() {
         return System.getProperty("os.name").toLowerCase().indexOf("mac") != -1;
     }
@@ -28,12 +29,29 @@ public class MacOSXHandler {
     public static void initialize(final OIMAdmin admin) {
         System.setProperty("apple.laf.useScreenMenuBar", "true");
         System.setProperty("com.apple.mrj.application.apple.menu.about.name", "sysadmin++");
-        com.apple.eawt.Application application = com.apple.eawt.Application.getApplication();
-        application.setQuitStrategy(com.apple.eawt.QuitStrategy.CLOSE_ALL_WINDOWS);
+        try {
+            Class applicationClass = Class.forName("com.apple.eawt.Application");
+            Class quitStrategyClass = Class.forName("com.apple.eawt.QuitStrategy");
+            Object application = applicationClass.getMethod("getApplication").invoke(null);
+            Object quitStrategyCloseAll = quitStrategyClass.getMethod("valueOf").invoke(null, "CLOSE_ALL_WINDOWS");
+            applicationClass.getMethod("setQuitStrategy", quitStrategyClass).invoke(application, quitStrategyCloseAll);
+        }catch (Exception exception) {
+            logger.warn("Failed to initialize on Mac Platform.", exception);
+        }
     }
 
-    public static void registerMenuBar(JMenuBar menuBar) {
-        com.apple.eawt.Application.getApplication().setDefaultMenuBar(menuBar);
+    public static void registerMenuBar(javax.swing.JMenuBar menuBar) {
+        try {
+            Class applicationClass = Class.forName("com.apple.eawt.Application");
+            Object application = applicationClass.getMethod("getApplication").invoke(null);
+            if (application != null) {
+                applicationClass.getMethod("setDefaultMenuBar", javax.swing.JMenuBar.class).invoke(application, menuBar);
+            } else {
+                logger.warn("Could not locate Application object on Mac platform.");
+            }
+        }catch (Exception exception) {
+            logger.warn("Failed to set Default Menu Bar.", exception);
+        }
     }
 
     public static void cleanup(final OIMAdmin admin) {
